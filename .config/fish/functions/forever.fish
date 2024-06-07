9f536c2bb542f84a714e6a1a20dfd27f8c2f367b
Daste <stefankar1000@gmail.com>
2024-06-07 15:31:39 +0200

    Add a `forever` fish function for running commands indifinitely

---
+++ .config/fish/functions/forever.fish
@@ -0,0 +1,24 @@
+function forever
+    if test -z "$argv"
+        echo "Usage: forever [-q] <command>"
+        return 1
+    end
+
+    argparse --stop-nonopt --name "forever" "q" -- $argv
+    or return
+
+    set verbose true
+    if test -n "$_flag_q"
+        set verbose false
+    end
+
+    while true
+        $verbose && echo "[forever] $argv"
+        $argv
+        set -l signal (fish_status_to_signal "$status")
+        if test $signal = "SIGINT"
+            $verbose && echo "[forever] Received SIGINT, exiting..."
+            break
+        end
+    end
+end
