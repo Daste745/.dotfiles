7d9f5f1e2c47b289b3d0c7fb9042757cde494f2c
Daste <stefankar1000@gmail.com>
2024-08-21 10:34:06 +0200

    Copy mise config to created tmp directories

--- .config/fish/functions/tmpdir.fish
+++ .config/fish/functions/tmpdir.fish
@@ -1,5 +1,15 @@
 function tmpdir
-set dirname "/tmp/$(random 1000000 9009999)"
-mkdir -p "$dirname"
-cd "$dirname"
+    set dirname "/tmp/$(random 1000000 9009999)"
+
+    mkdir -pv "$dirname"
+
+    # Copy .mise.toml or .rtx.toml if they exist
+    for file in '.mise.toml' '.rtx.toml'
+        if test -f "$HOME/$file"
+            cp -v "$HOME/$file" "$dirname"
+            break
+        end
+    end
+
+    cd "$dirname"
 end
