b9455a672aeecbfbbc479ed559a35571a1e0fcf1
Daste <stefankar1000@gmail.com>
2024-10-10 09:50:29 +0200

    fish: Add an abbr to activate a python venv
    It should look for suitable directory names and echo a correct
    `source` command when it finds one.

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -2,6 +2,15 @@ function last_history_item
 	echo $history[1]
 end
 
+function activate_venv
+    for venv_dir in "venv" ".venv"
+        if test -d "$venv_dir" -a -f "$venv_dir/bin/activate.fish"
+            echo "source $venv_dir/bin/activate.fish"
+            return
+        end
+    end
+    echo "# No venv found"
+end
 
 # Misc. aliases
 ## Last command, like !! in bash and zsh
@@ -11,6 +20,8 @@ abbr takeown sudo chown -v $(whoami):$(whoami)
 abbr e open .
 ## Shortcut to the dotfiles directory
 abbr dots z ~/.dotfiles
+# Activate a python virtual environment
+abbr acv --function activate_venv
 
 # Add verbose and interactive by default
 abbr cp cp -vi
