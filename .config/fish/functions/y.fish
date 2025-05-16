957f1cd1b510f5237f3896bbf89b61c0d83306f4
Daste <stefankar1000@gmail.com>
2025-05-17 01:40:38 +0200

    fish: Remove the `y` function

--- .config/fish/functions/y.fish
+++
@@ -1,9 +0,0 @@
-function y --wraps yazi
-    set tmp (mktemp -t 'yazi-cwd.XXXXXX')
-    yazi $argv --cwd-file="$tmp"
-    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-        __zoxide_cd_internal -- "$cwd"
-    end
-    rm -f -- "$tmp"
-end
-
